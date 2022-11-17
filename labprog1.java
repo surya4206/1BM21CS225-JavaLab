a0ef2fadec97768be6b517588fe37930c0962400
surya4206 <117361908+surya4206@users.noreply.github.com>
2022-11-17 15:00:05 +0530

    Update labprog1.java

--- labprog1.java
+++ labprog1.java
@@ -8,20 +8,24 @@ class labprog1{
         b=ss.nextDouble();
         c=ss.nextDouble();
         double dis=b*b-4*a*c;
+	  double r1=-b/(2*a);
+        double r2=(Math.sqrt(Math.abs(dis)))/(2*a);
         if(dis>0)
         {
             System.out.println("Roots are real and distinct.");
-            double r1=-b/(2*a);
-            double r2=(Math.sqrt(dis))/(2*a);
-            System.out.println("Solution 1="+(r1+r2));
-            System.out.println("Solution 2="+(r1-r2));
+            System.out.println("Solution 1="+String.format("%.2f",(r1+r2)));
+            System.out.println("Solution 2="+String.format("%.2f",(r1-r2)));
         }
         else if(dis==0)
         {
             System.out.println("Roots are real and equal.");
-            System.out.println("Solution="+(-b/(2*a)));
+            System.out.println("Solution="+r1);
         }
         else
+	  {
         System.out.println("No real solutions exist.");
+	  System.out.println("Imaginary solution 1="+r1+"+i"+String.format("%.2f",r2));
+	  System.out.println("Imaginary solution 2="+r1+"-i"+String.format("%.2f",r2));
+	  }
     }
 }
